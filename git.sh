d7a4a9cf00b6dc1028dc6cfa8e43ea05a81bc13a
Akash671 <akashsaini454545gmail.com>
2021-04-16 23:51:46 +0530

    linux script for github

--- git.sh
+++ git.sh
@@ -14,8 +14,8 @@ read -p "| Enter commit message |:|" message
 echo "$(tput setaf 10)+----------------------+ +---------------------+"
 git commit -m "$message"
 echo "$(tput setaf 41)Your message is : $message"
-
-git push
+read -p "Are you sure : " ANS
+gt push
 echo "$(tput setaf 31)+---------------------------------+"
 echo "$(tput setaf 31)|                                 |"
 echo "$(tput setaf 31)|    Thank You! Akash Kumar       |"
